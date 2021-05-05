8589a5a5efbd1d3a61fc102d18cde0b12ea5a80f
madderstone <anthony.issa@hotmail.fr>
2021-05-05 10:08:34 +0200

    test ImageView

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,32 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+    @FXML
+    private TilePane tilePane;
+
+    @FXML
+    private ImageView testImage;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
-    }
+        testImage.setImage(new Image(""));
+        tilePane  = new TilePane(Orientation.VERTICAL);
+        tilePane.setTileAlignment(Pos.CENTER_LEFT);
+        tilePane.setPrefRows(10);
+        for (int i = 0; i < 50; i++) {
+            tilePane.getChildren().add(new ImageView(new Image("")));
+        }}
 
 }
