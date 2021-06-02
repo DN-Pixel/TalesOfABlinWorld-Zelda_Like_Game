3767114f2a6ad8f0d12e0dd0967a83b90c95abb2
madderstone <anthony.issa@hotmail.fr>
2021-06-02 15:24:01 +0200

    tentative de fix de merge

---
+++ src/sample/modele/items/Armes/ArmeDistance.java
@@ -0,0 +1,10 @@
+package sample.modele.items.Armes;
+
+public class ArmeDistance extends Arme{
+
+    public ArmeDistance (String name, int degat) {
+        super(name, degat, 0);
+    }
+
+
+}
---
+++ src/sample/modele/items/Armes/Shuriken.java
@@ -0,0 +1,8 @@
+package sample.modele.items.Armes;
+
+public class Shuriken extends ArmeDistance{
+
+    public Shuriken () {
+        super("Shuriken", 6);
+    }
+}
